2c2f9a21f915c7fb49ef92759375f9691b6bb3ce
chaehyeon02 <rhee2015@gmail.com>
2025-06-03 04:11:11 +0900

    [FIX] 직접통신 url 수정

--- src/public/js/apiUrl.js
+++ src/public/js/apiUrl.js
@@ -3,8 +3,9 @@ export const apiUrl = 'http://uniunity.store/council';
 // 마이크로서비스별 API 주소
 export const baseUrls = {
   post: "http://uniunity.store/post",
-  postReaction: "http://uniunity.store/post",
+  postReaction: "http://uniunity.store/mypage",
   partner: "http://uniunity.store/partner",
-  user: "http://uniunity.store/auth",
+  auth: "http://uniunity.store/auth",
+  user: "http://uniunity.store/user",
   retailer: "http://uniunity.store/retailer"
 };
