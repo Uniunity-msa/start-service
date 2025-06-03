1f88aad64e4ac306b3b2ed81fcef406c19432550
chaehyeon02 <rhee2015@gmail.com>
2025-06-03 16:26:52 +0900

    [TEST] home.ctrl 로그 추가
    v0.9

--- src/controllers/home.ctrl.js
+++ src/controllers/home.ctrl.js
@@ -16,13 +16,15 @@ const council = {
     //간접통신으로 학교 이름 가져오기
     getUniversityName: async (req, res) => {
         try {
+            console.log("home getUniversityName 시작");
             const university_url = req.body.university_url;
+            console.log("home getUniversityName university_url: ", university_url);
             //rabbitMQ로 user-service에 university_name, id 요청
             await sendUniversityURL(university_url, 'SendUniversityName');
 
             //데이터 수신
             const university_name = await receiveUniversityData('RecvStartUniversityName');
-            console.log("home getUniversityName: ", university_name);
+            console.log("home getUniversityName result: ", university_name);
             return res.json(university_name);
 
         } catch (err) {
@@ -34,15 +36,23 @@ const council = {
     //통신해서 학교id, image_url, post_id 가져오기
     getUniversityID: async (req, res) => {
         try {
+            console.log("home getUniversityName 시작");
             const university_url = req.body.university_url;
+            console.log("home getUniversityName university_url: ", university_url);
             await sendUniversityURL(university_url, 'SendUniversityID');
             const university_id = await receiveUniversityData('RecvStartUniversityID');
             
+            if(university_id == null) {
+                console.log("id를 받아오지 못했습니다.");
+                return res.status(500).json({ error: 'Internal Server Error' }); 
+            }
+
+            console.log("home getUniversityName university_id: ", university_id);
             await sendUniversityID(university_id, 'SendPostList');
             const post_info = await receiveUniversityData('RecvPostList');
             const result = post_info.post_info;
-            console.log("home getUniversityID: ", result);
             
+            console.log("home getUniversityName result: ", result);
             return res.json({result});
         } catch (err) {
             console.log("getUniversityID error", err);
@@ -54,10 +64,11 @@ const council = {
         try { 
             console.log("home.ctrl의 getUniversityLocation ");
             const university_url = req.body.university_url;
+            console.log("home getUniversityName university_url: ", university_url);
             await sendUniversityURL(university_url, 'SendUniversityLocation');
             const university_location = await receiveUniversityData('RecvStartUniversityLocation');
 
-            console.log("home: university location:", university_location);
+            console.log("home getUniversityName university_location: ", university_location);
             return res.json(university_location);
         } catch (err) {
             console.error('getUniversityLocation error:', err);
