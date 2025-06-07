20c8d6616b753ca742e08a6c685573dde096d563
Jihyeon1520 <hyeon_0225@naver.com>
2025-06-07 23:43:16 +0900

    수정

--- src/controllers/home.ctrl.js
+++ src/controllers/home.ctrl.js
@@ -40,11 +40,6 @@ const council = {
             const university_id = await receiveUniversityData('RecvStartUniversityID', correlationId);
             console.log("getUniversityID => university_id: ", university_id);
             
-            if(!university_id || university_id.university_id == null) {
-                console.error("id를 받아오지 못했습니다.");
-                return res.status(500).json({ error: 'Internal Server Error' }); 
-            }
-            
             await sendUniversityID(university_id.university_id, 'SendPostList', correlationId);
             const post_info = await receiveUniversityData('RecvPostList', correlationId);
             console.log("getUniversityID => post_info: ", post_info);
