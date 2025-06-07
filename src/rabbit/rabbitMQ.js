247c4b1445764d9a8c0132da533000705938d3f4
chaehyeon02 <rhee2015@gmail.com>
2025-06-07 23:59:36 +0900

    [FIX] 콘솔 object 수정

--- src/rabbit/rabbitMQ.js
+++ src/rabbit/rabbitMQ.js
@@ -89,7 +89,8 @@ async function receiveUniversityData(queueName, correlationId) {
     if (msg) {
       const data = JSON.parse(msg.content.toString());
       console.log("msg: ", msg);
-      console.log("data: ", data);
+      // console.log("data: ", data);
+      console.dir("data2: ", data, { depth: null });
       // 요청 ID(correlationId)를 확인하여 응답을 매칭
       if (data.correlationId === correlationId) {
         channel.ack(msg);  // 처리 완료된 메시지에 대해 ack
