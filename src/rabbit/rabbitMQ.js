302bb8405cf552905b751cdd4fdef4809655b440
chaehyeon02 <rhee2015@gmail.com>
2025-06-08 00:11:21 +0900

    [FIX] console.dir뺌

--- src/rabbit/rabbitMQ.js
+++ src/rabbit/rabbitMQ.js
@@ -89,8 +89,7 @@ async function receiveUniversityData(queueName, correlationId) {
     if (msg) {
       const data = JSON.parse(msg.content.toString());
       console.log("msg: ", msg);
-      // console.log("data: ", data);
-      console.dir(data, { depth: null });
+      console.log("data: ", data);
       // 요청 ID(correlationId)를 확인하여 응답을 매칭
       if (data.correlationId === correlationId) {
         channel.ack(msg);  // 처리 완료된 메시지에 대해 ack
